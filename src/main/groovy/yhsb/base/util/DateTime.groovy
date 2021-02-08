afb8cd0b96cae4b9e8c327e3b164d606fd55b35d
John Wall <5672618@qq.com>
2021-02-08 20:58:32 +0800

    Update

--- src/main/groovy/yhsb/base/util/DateTime.groovy
+++ src/main/groovy/yhsb/base/util/DateTime.groovy
@@ -51,12 +51,37 @@ class YearMonth implements Comparable<YearMonth> {
         new YearMonth(year + y, m)
     }
 
+    YearMonth offsetInPlace(int months) {
+        months = month + months
+        var y = months.intdiv(12)
+        var m = months % 12
+        if (m <= 0) {
+            y -= 1
+            m += 12
+        }
+        year += y
+        month = m
+        this
+    }
+
     static YearMonth from(int yearMonth) {
         new YearMonth(yearMonth.intdiv(100), yearMonth % 100)
     }
 
     int compareTo(YearMonth that) {
         (this.year * 12 + this.month) - (that.year * 12 + that.month)
+    }
+
+    YearMonth max(YearMonth that) {
+        if (this > that) this else that
+    }
+
+    YearMonth min(YearMonth that) {
+        if (this < that) this else that
+    }
+
+    YearMonth copy() {
+        new YearMonth(this.year, this.month)
     }
 
     @Override
@@ -73,5 +98,105 @@ class YearMonth implements Comparable<YearMonth> {
     @Override
     String toString() {
         String.format('%04d%02d', year, month)
+    }
+}
+
+class YearMonthRange {
+    YearMonth start
+    YearMonth end
+
+    YearMonthRange(YearMonth start, YearMonth end) {
+        if (start > end)
+            throw new IllegalArgumentException('start must be less than or equal end')
+
+        this.start = start
+        this.end = end
+    }
+
+    List<YearMonthRange> minus(YearMonthRange that) {
+        if (that.end < this.start || this.end < that.start) {
+            [this]
+        } else if (that.start <= this.start) {
+            if (that.end < this.end) {
+                [new YearMonthRange(that.end.offset(1), this.end)]
+            } else {
+                []
+            }
+        } else { // that.start > this.start
+            if (that.end < this.end) {
+                [new YearMonthRange(this.start, that.start.offset(-1)), new YearMonthRange(that.end.offset(1), this.end)]
+            } else {
+                [new YearMonthRange(this.start, that.start.offset(-1))]
+            }
+        }
+    }
+
+    List<YearMonthRange> minus(List<YearMonthRange> those) {
+        subtract([this], those)
+    }
+
+    static List<YearMonthRange> subtract(List<YearMonthRange> these, List<YearMonthRange> those) {
+        those.each { that ->
+            these = these.flatten {
+                it - that
+            }
+        }
+        these
+    }
+
+    int getMonths() {
+        (end.year * 12 + end.month) - (start.year * 12 + start.month) + 1
+    }
+
+    YearMonthRange copy() {
+        new YearMonthRange(this.start.copy(), this.end.copy())
+    }
+
+    @Override
+    String toString() {
+        "$start-$end"
+    }
+}
+
+class DateTimeExtensions {
+    static int getMonths(List<YearMonthRange> these) {
+        var total = 0
+        these.each {
+            total += it.months
+        }
+        total
+    }
+
+    static LinkedList<YearMonthRange> offsetInPlace(LinkedList<YearMonthRange> these, int months) {
+        if (these.empty) return these
+        var first = these.first()
+        var firstMonths = first.months
+        if (firstMonths > months) {
+            first.start.offsetInPlace(months)
+        } else {
+            these.remove(0)
+            if (firstMonths < months) {
+                these.offsetInPlace(months - firstMonths)
+            }
+        }
+        these
+    }
+
+    static List<LinkedList<YearMonthRange>> splitInPlace(LinkedList<YearMonthRange> these, int months) {
+        if (these.empty) return [[], []]
+        var first = these.first()
+        var firstMonths = first.months
+        if (firstMonths > months) {
+            [[new YearMonthRange(first.start, first.start.offset(months - 1))],
+             these.offsetInPlace(months)]
+        } else {
+            first = these.removeFirst()
+            if (firstMonths == months) {
+                [[first], these]
+            } else {
+                var list = these.splitInPlace(months - firstMonths)
+                [list[0].addFirst(first), list[1]]
+            }
+        }
     }
 }
--- src/test/groovy/yhsb/base/util/DateTime.groovy
+++ src/test/groovy/yhsb/base/util/DateTime.groovy
@@ -17,4 +17,30 @@ void testYearMonth() {
     println map
 }
 
+void testYearMonthRange() {
+    var birthDay = new YearMonth(1990, 12)
+    var boughtSpans = [
+            new YearMonthRange(new YearMonth(2008, 1), new YearMonth(2009, 9)),
+            new YearMonthRange(new YearMonth(2010, 2), new YearMonth(2010, 4)),
+    ]
+
+    var totalMonths = 180
+    var bonusMonths = 48
+
+    var startMonth = birthDay.offset(16 * 12 + 1).max(new YearMonth(2005, 8))
+    var endMonth = new YearMonth(2020, 7)
+
+    var validBound = new YearMonthRange(startMonth, endMonth)
+    println "可以缴费年限: $validBound"
+
+    println "已经缴费年限: ${boughtSpans.join('|')}"
+
+    var validBounds = validBound - boughtSpans
+    var canBuyMonths = validBounds.months
+
+    println "尚未缴费年限: ${validBounds.join('|')}|共计${canBuyMonths}个月"
+
+
+}
+
 testYearMonth()
